f1837860d75141fed8c901405009de3840070966
Lucky Raj <lluckyadarshrajkl@gmail.com>
2024-09-03 17:14:33 +0530

    project initiated

---
+++ src/test/java/com/jsp/rest/ets/EduTrackingSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.rest.ets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackingSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
