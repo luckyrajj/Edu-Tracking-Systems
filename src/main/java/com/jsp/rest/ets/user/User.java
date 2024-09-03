fd0ccfbbb88fea70baf1ee8bf605caaa3fb94a1f
Lucky Raj <lluckyadarshrajkl@gmail.com>
2024-09-03 17:29:46 +0530

    defined user entity structure

--- src/main/java/com/jsp/rest/ets/user/User.java
+++ src/main/java/com/jsp/rest/ets/user/User.java
@@ -2,6 +2,7 @@ package com.jsp.rest.ets.user;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -13,13 +14,18 @@ import lombok.Setter;
 @Table(name = "users")
 public class User {
 
+	@Column(name = "user_id")
 	private String userId;
-	
+	@Column(name = "user_name")
 	private String userName;
+	@Column(name = "user_email")
 	private String email;
+	@Column(name = "user_password")
 	private String password;
-	//private UserRole role;
-	
+	@Column(name = "user_role")
+	private String role;
+	@Column(name = "created_at")
 	private LocalDateTime createdAt;
+	@Column(name = "modified_at")
 	private LocalDateTime modifiedAt;
 }
