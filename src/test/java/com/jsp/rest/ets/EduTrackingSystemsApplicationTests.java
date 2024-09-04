09a6f74b5ac9b316ab6cfd3ce22cae6b64878d32
Lucky Raj <lluckyadarshrajkl@gmail.com>
2024-09-04 11:31:33 +0530

    updated Test class

--- src/test/java/com/jsp/rest/ets/EduTrackingSystemsApplicationTests.java
+++ src/test/java/com/jsp/rest/ets/EduTrackingSystemsApplicationTests.java
@@ -3,7 +3,7 @@ package com.jsp.rest.ets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {EduTrackingSystemsApplication.class})
+//@SpringBootTest(classes = {EduTrackingSystemsApplication.class})
 class EduTrackingSystemsApplicationTests {
 
 	@Test
