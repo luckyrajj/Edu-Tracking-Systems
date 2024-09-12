a90c949256ea3328f376ec2f1b0fd70aa2b51898
Lucky Raj <lluckyadarshrajkl@gmail.com>
2024-09-12 13:13:42 +0530

    updated the verification url should be public

--- src/main/java/com/jsp/rest/ets/security/SecurityConfig.java
+++ src/main/java/com/jsp/rest/ets/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,9 +16,9 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-       return httpSecurity.csrf(csrf->csrf.disable())
+       return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(authorize->authorize
-                            .requestMatchers("/register/**","/login")
+                            .requestMatchers("/register/**","/login","verify/users/register")
                             .permitAll()
                             .anyRequest()
                             .authenticated())
@@ -26,6 +27,8 @@ public class SecurityConfig {
                 .build();
 
     }
+
+
 
 
 }
