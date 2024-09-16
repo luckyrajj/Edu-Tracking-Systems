bbff05593d89439e17d5d3e7c13eed0ec7fe5585
Lucky Raj <lluckyadarshrajkl@gmail.com>
2024-09-16 12:14:31 +0530

    created a method to createJwtToken

--- src/main/java/com/jsp/rest/ets/security/JWTService.java
+++ src/main/java/com/jsp/rest/ets/security/JWTService.java
@@ -1,7 +1,15 @@
 package com.jsp.rest.ets.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class JWTService {
@@ -11,6 +19,20 @@ public class JWTService {
     private String secret;
 
     @Value("${my_app.jwt.access_expiry}")
-    private int access_expiry;
+    private long access_expiry;
+
+     public String createJwtToken(String userId,String email,String role){
+         return Jwts.builder()
+                 .setClaims(Map.of("userId",userId,"email",email,"role",role))
+                 .setIssuedAt(new Date(System.currentTimeMillis()))
+                 .setExpiration(new Date(System.currentTimeMillis()+access_expiry*60*1000))
+                 .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
+
+
+     }
+
+     private Key getSignInKey(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+     }
 
 }
